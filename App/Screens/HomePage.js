3af11eb0e94e954a80144f1a34af83dfcbbd6bab
AurelSondakh <aurelsondakh@gmail.com>
2024-07-17 16:58:05 +0700

    feat: HomePage, Gradient Background

---
+++ App/Screens/HomePage.js
@@ -0,0 +1,292 @@
+/* eslint-disable prettier/prettier */
+import React, {useState} from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  FlatList,
+  Image,
+  Dimensions,
+  ScrollView,
+} from 'react-native';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import Entypo from 'react-native-vector-icons/Entypo';
+import CardData from '../Data/CardData';
+import PaymentHistoryData from '../Data/PaymentHistoryData';
+import Container from '../Components/Container';
+
+const width = Dimensions.get('screen').width;
+
+const HomePage = () => {
+  const [showMoney, setShowMoney] = useState(true);
+
+  const handleToggleShowMoney = () => {
+    if (showMoney) setShowMoney(false);
+    else setShowMoney(true);
+  };
+
+  const cardListComponent = ({item}) => {
+    let imageSource = item.cardImage;
+    return (
+      <View style={styles.cardContainer}>
+        <View style={styles.cardImageContainer}>
+          <Image
+            style={styles.cardImage}
+            source={imageSource}
+            resizeMode="contain"
+          />
+          <Entypo name={'dots-three-horizontal'} color={'#FFF'} size={18} />
+        </View>
+        <View style={styles.cardInformationContainer}>
+          <Text style={styles.regularText}>Balance</Text>
+          <Text style={styles.amountText}>$ {item.totalBalance}</Text>
+          <Text style={[styles.regularText, {fontSize: 10}]}>
+            {item.cardName} - {item.cardNumber}
+          </Text>
+        </View>
+      </View>
+    );
+  };
+
+  const paymentListComponent = ({item}) => {
+    let imageSource = item.paymentProductImage;
+    return (
+      <View style={styles.paymentListContainer}>
+        <View>
+          <Image
+            style={styles.paymentImage}
+            source={imageSource}
+            resizeMode="contain"
+          />
+        </View>
+        <View style={styles.paymentProductContainer}>
+          <Text style={styles.mediumText}>{item.productName}</Text>
+          <Text style={[styles.regularText, {fontSize: 13, color: '#646E72'}]}>{item.paymentPurpose}</Text>
+        </View>
+        <View>
+          <Text style={[styles.mediumText, {color: item.paymentType === 'income' ? '#7CE8A8' : '#F9797A' }]}>
+            {item.paymentType === 'income'
+              ? `$${item.totalPayment}`
+              : `-$${item.totalPayment}`}
+          </Text>
+          <Text style={[styles.regularText, {fontSize: 13, color: '#646E72'}]}>{item.paymentDate}</Text>
+        </View>
+      </View>
+    );
+  };
+
+  return (
+    <View style={styles.homeContainer}>
+      <Container />
+      <ScrollView>
+        <View style={styles.headerContainer}>
+          <TouchableOpacity style={styles.scanQrButton}>
+            <MaterialIcons name={'qr-code-scanner'} color={'#FFF'} size={28} />
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.scanNotifButton}>
+            <MaterialIcons name={'notifications'} color={'#FFF'} size={28} />
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <MaterialIcons name={'search'} color={'#FFF'} size={28} />
+          </TouchableOpacity>
+        </View>
+        <View style={styles.totalAmountContainer}>
+          <View style={styles.availableMoneyContainer}>
+            <Text style={styles.regularText}>Total Available Money</Text>
+            <Text style={styles.amountText}>
+              {showMoney ? '$ 7,483.54' : '******'}
+            </Text>
+          </View>
+          <TouchableOpacity
+            onPress={handleToggleShowMoney}
+            style={styles.hideBalanceContainer}>
+            <FontAwesome
+              style={{marginRight: 10}}
+              name={showMoney ? 'eye-slash' : 'eye'}
+              color={'#FFF'}
+              size={24}
+            />
+            <Text style={styles.regularText}>
+              {showMoney ? 'Hide' : 'Show'} Balance
+            </Text>
+          </TouchableOpacity>
+        </View>
+        <View>
+          <FlatList
+            horizontal={true}
+            data={CardData}
+            renderItem={(item, index) => cardListComponent(item)}
+            keyExtractor={item => item?.cardId}
+          />
+        </View>
+        <View style={styles.expencesContainer}>
+          <Text style={styles.regularText}>December Expences</Text>
+          <Text style={styles.amountText}>$ 3,478.48</Text>
+          <View style={styles.expencesLineContainer}>
+            <View style={styles.firstExpences} />
+            <View style={styles.secondExpences} />
+            <View style={styles.thirdExpences} />
+            <View style={styles.forthExpences} />
+            <View style={styles.fifthExpences} />
+          </View>
+        </View>
+        <View style={styles.paymentHistoryContainer}>
+          <View style={styles.paymentHistoryHeader}>
+            <Text style={styles.paymentHistoryTitle}>Payment History</Text>
+            <MaterialIcons name={'chevron-right'} color={'#FFF'} size={32} />
+          </View>
+          <View>
+            <FlatList
+              data={PaymentHistoryData}
+              renderItem={(item, index) => paymentListComponent(item)}
+              keyExtractor={item => item?.paymentId}
+            />
+          </View>
+        </View>
+      </ScrollView>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  homeContainer: {
+    flex: 1,
+    backgroundColor: '#070A0E',
+    paddingTop: 60,
+    paddingHorizontal: 10,
+  },
+  headerContainer: {
+    paddingHorizontal: 10,
+    flexDirection: 'row',
+  },
+  scanQrButton: {
+    flex: 1,
+  },
+  scanNotifButton: {
+    marginRight: 20,
+  },
+  totalAmountContainer: {
+    paddingHorizontal: 10,
+    flexDirection: 'row',
+    marginTop: 35,
+    justifyContent: 'space-between',
+  },
+  availableMoneyContainer: {},
+  hideBalanceContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  regularText: {
+    color: '#FFF',
+    fontFamily: 'Poppins-Regular',
+  },
+  amountText: {
+    color: '#FFF',
+    fontFamily: 'Poppins-Bold',
+    fontSize: 24,
+  },
+  cardContainer: {
+    backgroundColor: '#0E141A',
+    marginRight: 10,
+    width: width / 2.2,
+    borderRadius: 15,
+    paddingBottom: 20,
+    marginTop: 20,
+  },
+  cardImageContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingRight: 30,
+    paddingLeft: 10,
+    borderBottomWidth: 2,
+    borderBottomColor: '#12181E',
+  },
+  cardImage: {
+    width: 75,
+    height: 75,
+  },
+  cardInformationContainer: {
+    paddingLeft: 20,
+    marginTop: 30,
+  },
+  expencesContainer: {
+    padding: 25,
+    backgroundColor: '#0E141A',
+    borderRadius: 15,
+    marginTop: 20,
+  },
+  expencesLineContainer: {
+    flexDirection: 'row',
+    marginTop: 15,
+  },
+  firstExpences: {
+    borderWidth: 4,
+    width: width / 5,
+    borderColor: '#FFE14F',
+  },
+  secondExpences: {
+    borderWidth: 4,
+    width: width / 5,
+    borderColor: '#CA72E9',
+  },
+  thirdExpences: {
+    borderWidth: 4,
+    width: width / 10,
+    borderColor: '#338FFF',
+  },
+  forthExpences: {
+    borderWidth: 4,
+    width: width / 6,
+    borderColor: '#2AC46A',
+  },
+  fifthExpences: {
+    borderWidth: 4,
+    width: width / 7,
+    borderColor: '#4A5356',
+  },
+  paymentHistoryContainer: {
+    backgroundColor: '#0E141A',
+    borderRadius: 15,
+    marginTop: 10,
+    marginBottom: 80
+  },
+  paymentHistoryHeader: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingLeft: 25,
+    paddingRight: 15,
+    paddingTop: 20,
+  },
+  paymentHistoryTitle: {
+    color: '#FFF',
+    fontSize: 14,
+    fontFamily: 'Poppins-Medium',
+  },
+  paymentListContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingLeft: 10,
+    paddingRight: 20,
+    borderBottomWidth: 2,
+    paddingVertical: 10,
+    borderBottomColor: '#171D22'
+  },
+  paymentImage: {
+    width: 75,
+    height: 75,
+  },
+  paymentProductContainer: {
+    flex: 1
+  },
+  mediumText: {
+    fontFamily: 'Poppins-Medium',
+    fontSize: 14,
+    color: '#FFF'
+  }
+});
+
+export default HomePage;
---
+++ App/Screens/LoginPage.js
@@ -0,0 +1,148 @@
+/* eslint-disable prettier/prettier */
+import React, {useState} from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  StatusBar,
+  TouchableOpacity,
+} from 'react-native';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import {useNavigation} from '@react-navigation/native';
+import Container from '../Components/Container';
+
+const LoginPage = () => {
+  const [usernameTextInputIsActive, setUsernameTextInputIsActive] =
+    useState(false);
+  const [passwordTextInputIsActive, setPasswordTextInputIsActive] =
+    useState(false);
+  const [showPassword, setShowPassword] = useState(false);
+  const navigation = useNavigation();
+
+  const handleToggleShowPassword = () => {
+    if (showPassword) setShowPassword(false);
+    else setShowPassword(true);
+  };
+
+  return (
+    <>
+      <View style={styles.container}>
+        <Container />
+        <StatusBar translucent backgroundColor="transparent" />
+        <View>
+          <Text style={styles.title}>Welcome Back</Text>
+          <Text style={styles.information}>Enter your login details</Text>
+          <View
+            style={[
+              styles.textInputContainer,
+              {borderColor: usernameTextInputIsActive ? '#F2CF4B' : '#11181E'},
+            ]}>
+            <FontAwesome name={'user'} color={'#FFF'} size={24} />
+            <TextInput
+              placeholder="Username"
+              placeholderTextColor={'#FFF'}
+              style={styles.textInput}
+              onFocus={() => setUsernameTextInputIsActive(true)}
+              onEndEditing={() => setUsernameTextInputIsActive(false)}
+            />
+          </View>
+          <View
+            style={[
+              styles.textInputContainer,
+              {
+                borderColor: passwordTextInputIsActive ? '#F2CF4B' : '#11181E',
+                marginTop: 15,
+              },
+            ]}>
+            <FontAwesome name={'lock'} color={'#FFF'} size={24} />
+            <TextInput
+              placeholder="Password"
+              placeholderTextColor={'#FFF'}
+              style={styles.textInput}
+              secureTextEntry={!showPassword}
+              onFocus={() => setPasswordTextInputIsActive(true)}
+              onEndEditing={() => setPasswordTextInputIsActive(false)}
+            />
+            <TouchableOpacity onPress={handleToggleShowPassword}>
+              <FontAwesome
+                name={showPassword ? 'eye-slash' : 'eye'}
+                color={'#FFF'}
+                size={24}
+              />
+            </TouchableOpacity>
+          </View>
+          <TouchableOpacity
+            onPress={() => navigation.navigate('BottomTabNavigator')}
+            style={styles.loginButtonContainer}>
+            <Text style={styles.loginButtonText}>Login</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.forgotPassContainer}>
+            <Text style={styles.forgotPassText}>Forgot login details?</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: '#070A0E',
+  },
+  title: {
+    marginTop: 50,
+    fontSize: 36,
+    fontFamily: 'Poppins-SemiBold',
+    color: '#FFF',
+    marginBottom: 15,
+    textAlign: 'center',
+  },
+  information: {
+    fontFamily: 'Poppins-Regular',
+    color: '#FFF',
+    marginBottom: 35,
+    textAlign: 'center',
+  },
+  textInputContainer: {
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    borderWidth: 1,
+    backgroundColor: '#0B1014',
+    marginHorizontal: 20,
+    borderRadius: 30,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  textInput: {
+    marginHorizontal: 10,
+    color: '#FFF',
+    flex: 1,
+  },
+  loginButtonContainer: {
+    marginTop: 30,
+    paddingHorizontal: 20,
+    paddingVertical: 20,
+    backgroundColor: '#E9C749',
+    borderRadius: 30,
+    marginHorizontal: 20,
+  },
+  loginButtonText: {
+    textAlign: 'center',
+    color: '#000',
+    fontFamily: 'Poppins-SemiBold',
+    fontSize: 16,
+  },
+  forgotPassContainer: {
+    marginTop: 40,
+    alignItems: 'center',
+  },
+  forgotPassText: {
+    color: '#FFF',
+    fontFamily: 'Poppins-SemiBold',
+  },
+});
+
+export default LoginPage;
